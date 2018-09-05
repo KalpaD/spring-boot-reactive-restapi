9099c4312b1f8aa9c728ce1f5e3f2366161842f8
Kalpa Senanayak <senanayake.kalpa@gmail.com>
2018-09-05 15:27:17 +1000

    Adding git ignore and initial project structure

---
+++ server/src/main/java/com/kds/boot/reactive/reactiverest/ReactiveRestApplication.java
@@ -0,0 +1,12 @@
+package com.kds.boot.reactive.reactiverest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRestApplication.class, args);
+	}
+}
